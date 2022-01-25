71d742666b4af6e8033c2d2e1dec09cd525257ac
Lya6 <liasnikov99@gmail.com>
2022-01-25 14:29:47 +0300

    Session 1 code

--- app/src/main/java/com/example/p50_4/MainActivity.java
+++ app/src/main/java/com/example/p50_4/MainActivity.java
@@ -11,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    //Session1 code
 }
